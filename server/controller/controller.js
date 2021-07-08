b73b9e6c7d36b31142f35619ce780f9f7597cf15
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-07-08 22:54:40 +0530

    image uploaded

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,9 @@
+const UploadModel = require('../model/schema');
+const fs = require('fs');
 
-exports.home = (req, res)=>{
-    res.render('main');
+exports.home = async(req, res)=>{
+    const all_images = await UploadModel.find();
+    res.render('main', {images: all_images});
 }
 
 exports.uploads = (req, res, next)=>{
@@ -10,5 +13,44 @@ exports.uploads = (req, res, next)=>{
         return res.status(400).send('please choose files');
     }
 
-    res.json(files)
+    //convert images into base64 encoding
+    let imgArray = files.map((file)=>{
+        let img = fs.readFileSync(file.path)
+        
+        return encode_imgage = img.toString('base64');
+    }) 
+
+   let result = imgArray.map((src, index)=>{
+        //create object to store data in the collection
+        let finalImg ={
+            filename: files[index].originalname,
+            contentType: files[index].mimetype,
+            imageBase64: src
+        }
+
+        let newUpload = new UploadModel(finalImg);
+        return newUpload
+            .save()
+            .then(()=>{
+                return({msg:`${files[index].originalname}image uploaded succesfully`})
+            })
+            .catch(error=>{
+                if(error){
+                    if(error.anme === 'MongoError' && error.code === 11000){
+                        return Promise.reject({error: `Duplicate ${files[index].originalname}. File Already exists`})
+                    }
+                    return Promise.reject({error: error.message || `Cannot Upload ${files[index].originalname} Something missing`})
+                }
+            })
+    });
+
+    Promise.all(result)
+        .then(msg=>{
+            res.redirect('/')
+        })
+        .catch(err=>{
+            res.json(err)
+        })
+
+    // res.json(imgArray)
 }
